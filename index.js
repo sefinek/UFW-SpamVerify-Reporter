b154b19310f21b1296640a752683c0c9cc7588ff
Sefinek <contact@sefinek.net>
2025-04-29 06:31:39 +0200

    0.1.4.beta.1

--- index.js
+++ index.js
@@ -3,108 +3,74 @@
 
 const fs = require('node:fs');
 const chokidar = require('chokidar');
-const isLocalIP = require('./scripts/utils/isLocalIP.js');
-const parseTimestamp = require('./scripts/utils/parseTimestamp.js');
-const log = require('./scripts/utils/log.js');
-const { post } = require('./scripts/services/axios.js');
+const { parseUfwLog } = require('ufw-log-parser');
+const axios = require('./scripts/services/axios.js');
 const { reportedIPs, loadReportedIPs, saveReportedIPs, isIPReportedRecently, markIPAsReported } = require('./scripts/services/cache.js');
 const { refreshServerIPs, getServerIPs } = require('./scripts/services/ipFetcher.js');
+const { name, version, authorEmailWebsite, repoFullUrl } = require('./scripts/repo.js');
 const sendWebhook = require('./scripts/services/discordWebhooks.js');
+const isLocalIP = require('./scripts/isLocalIP.js');
+const log = require('./scripts/log.js');
 const config = require('./config.js');
-const { version } = require('./package.json');
-const { UFW_LOG_FILE, SPAMVERIFY_API_KEY, SERVER_ID, AUTO_UPDATE_ENABLED, AUTO_UPDATE_SCHEDULE, DISCORD_WEBHOOKS_ENABLED, DISCORD_WEBHOOKS_URL } = config.MAIN;
+const { UFW_LOG_FILE, ABUSEIPDB_API_KEY, SERVER_ID, EXTENDED_LOGS, AUTO_UPDATE_ENABLED, AUTO_UPDATE_SCHEDULE, DISCORD_WEBHOOKS_ENABLED, DISCORD_WEBHOOKS_URL } = config.MAIN;
 
 let fileOffset = 0;
 
-const reportIp = async (data, categories, comment) => {
+const reportIp = async ({ srcIp, dpt = 'N/A', proto = 'N/A', id, timestamp }, categories, comment) => {
+	if (!srcIp) return log('Missing source IP (srcIp)', 3);
+
+	if (getServerIPs().includes(srcIp)) return;
+	if (isIPReportedRecently(srcIp)) return;
+
 	try {
-		const { data: res } = await post('https://api.spamverify.com/v1/ip/report', {
-			ip_address: data.srcIp,
+		const { data: res } = await axios.post('/report', new URLSearchParams({
+			ip: srcIp,
 			categories,
 			comment,
-		}, { headers: { 'Api-Key': SPAMVERIFY_API_KEY } });
+		}), { headers: { 'Key': ABUSEIPDB_API_KEY } });
 
-		log(0, `Reported ${data.srcIp} [${data.dpt}/${data.proto}]; ID: ${data.id}; Categories: ${categories}; Abuse: ${res.data.threat_score}%`);
+		log(`Reported ${srcIp} [${dpt}/${proto}]; ID: ${id}; Categories: ${categories}; Abuse: ${res.data.abuseConfidenceScore}%`, 1);
 		return true;
 	} catch (err) {
-		log(
-			err.response?.status === 429 ? 0 : 2,
-			`Failed to report ${data.srcIp} [${data.dpt}/${data.proto}]; ID: ${data.id}; ${err.response?.data ? `\n${JSON.stringify(err.response.data)}` : err.message}`,
-			err.response?.status === 429 ? 0 : 1
-		);
-		return false;
+		const status = err.response?.status ?? 'unknown';
+		log(`Failed to report ${srcIp} [${dpt}/${proto}]; ${err.response?.data?.errors ? JSON.stringify(err.response.data.errors) : err.message}`, status === 429 ? 0 : 3);
 	}
-};
-
-const toNumber = (str, regex) => {
-	const parsed = str.match(regex)?.[1];
-	return parsed ? Number(parsed) : parsed;
 };
 
 const processLogLine = async (line, test = false) => {
-	if (!line.includes('[UFW BLOCK]')) return log(1, `Ignoring invalid line: ${line}`, 1);
+	if (!line.includes('[UFW BLOCK]')) return log(`Ignoring invalid line: ${line}`, 2);
 
-	const data = {
-		date: parseTimestamp(line), // Log timestamp
-		srcIp: line.match(/SRC=([\d.]+)/)?.[1] || null, // Source IP address
-		dstIp: line.match(/DST=([\d.]+)/)?.[1] || null, // Destination IP address
-		proto: line.match(/PROTO=(\S+)/)?.[1] || null, // Protocol (TCP, UDP, etc.)
-		spt: toNumber(line, /SPT=(\d+)/), // Source port
-		dpt: toNumber(line, /DPT=(\d+)/), // Destination port
-		in: line.match(/IN=(\w+)/)?.[1] || null, // Input interface
-		out: line.match(/OUT=(\w+)/)?.[1] || null, // Output interface
-		mac: line.match(/MAC=([\w:]+)/)?.[1] || null, // MAC address
-		len: toNumber(line, /LEN=(\d+)/), // Packet length
-		ttl: toNumber(line, /TTL=(\d+)/), // Time to live
-		id: toNumber(line, /ID=(\d+)/), // Packet ID
-		tos: line.match(/TOS=(\S+)/)?.[1] || null, // Type of service
-		prec: line.match(/PREC=(\S+)/)?.[1] || null, // Precedence
-		res: line.match(/RES=(\S+)/)?.[1] || null, // Reserved bits
-		window: toNumber(line, /WINDOW=(\d+)/), // TCP Window size
-		urgp: toNumber(line, /URGP=(\d+)/), // Urgent pointer
-		ack: !!line.includes('ACK'), // ACK flag
-		syn: !!line.includes('SYN'), // SYN flag
-	};
-
+	const data = parseUfwLog(line);
 	const { srcIp, proto, dpt } = data;
-	if (!srcIp) return log(2, `Missing SRC in the log line: ${line}`, 1);
+	if (!srcIp) return log(`Missing SRC in the log line: ${line}`, 3);
 
 	const ips = getServerIPs();
-	if (!Array.isArray(ips)) return log(2, 'For some reason, \'ips\' is not an array', 1);
+	if (!Array.isArray(ips)) return log(`For some reason, 'ips' from 'getServerIPs()' is not an array. Received: ${ips}`, 3, true);
 
-	if (ips.includes(srcIp)) {
-		return log(0, `Ignoring own IP address! PROTO=${proto?.toLowerCase()} SRC=${srcIp} DPT=${dpt} ID=${data.id}`, 1);
-	}
-
-	if (isLocalIP(srcIp)) {
-		return log(0, `Ignoring local IP address! PROTO=${proto?.toLowerCase()} SRC=${srcIp} DPT=${dpt} ID=${data.id}`, 1);
-	}
-
-	// Only TCP traffic can be reported.
+	if (ips.includes(srcIp)) return log(`Ignoring own IP address: PROTO=${proto?.toLowerCase()} SRC=${srcIp} DPT=${dpt} ID=${data.id}`, 2, true);
+	if (isLocalIP(srcIp)) return log(`Ignoring local IP address: PROTO=${proto?.toLowerCase()} SRC=${srcIp} DPT=${dpt} ID=${data.id}`, 2, true);
 	if (proto === 'UDP') {
-		return log(0, `Skipping UDP traffic: SRC=${srcIp} DPT=${dpt}`);
+		if (EXTENDED_LOGS) log(`Skipping UDP traffic: SRC=${srcIp} DPT=${dpt} ID=${data.id}`);
+		return;
 	}
 
-	// Tests
 	if (test) return data;
 
 	if (isIPReportedRecently(srcIp)) {
 		const lastReportedTime = reportedIPs.get(srcIp);
 		const elapsedTime = Math.floor(Date.now() / 1000 - lastReportedTime);
-
 		const days = Math.floor(elapsedTime / 86400);
 		const hours = Math.floor((elapsedTime % 86400) / 3600);
 		const minutes = Math.floor((elapsedTime % 3600) / 60);
 		const seconds = elapsedTime % 60;
-
 		const timeAgo = [
 			days && `${days}d`,
 			hours && `${hours}h`,
 			minutes && `${minutes}m`,
-			(seconds || !days && !hours && !minutes) && `${seconds}s`,
+			(seconds || (!days && !hours && !minutes)) && `${seconds}s`,
 		].filter(Boolean).join(' ');
 
-		log(0, `${srcIp} was last reported on ${new Date(lastReportedTime * 1000).toLocaleString()} (${timeAgo} ago)`);
+		if (EXTENDED_LOGS) log(`${srcIp} was last reported on ${new Date(lastReportedTime * 1000).toLocaleString()} (${timeAgo} ago)`);
 		return;
 	}
 
@@ -118,16 +84,16 @@ const processLogLine = async (line, test = false) => {
 };
 
 (async () => {
-	log(0, `Version ${version} - https://github.com/sefinek/UFW-SpamVerify-Reporter`);
+	log(`${repoFullUrl} - v${version} | Author: ${authorEmailWebsite}`);
 
 	loadReportedIPs();
 
-	log(0, 'Trying to fetch your IPv4 and IPv6 address from api.sefinek.net...');
+	log('Trying to fetch your IPv4 and IPv6 address from api.sefinek.net...');
 	await refreshServerIPs();
-	log(0, `Fetched ${getServerIPs()?.length} of your IP addresses. If any of them accidentally appear in the UFW logs, they will be ignored.`);
+	log(`Fetched ${getServerIPs()?.length} of your IP addresses. If any of them accidentally appear in the UFW logs, they will be ignored.`, 1);
 
 	if (!fs.existsSync(UFW_LOG_FILE)) {
-		log(2, `Log file ${UFW_LOG_FILE} does not exist`);
+		log(`Log file ${UFW_LOG_FILE} does not exist`, 3);
 		return;
 	}
 
@@ -138,7 +104,7 @@ const processLogLine = async (line, test = false) => {
 			const stats = fs.statSync(path);
 			if (stats.size < fileOffset) {
 				fileOffset = 0;
-				log(1, 'The file has been truncated, and the offset has been reset', 1);
+				log('The file has been truncated, and the offset has been reset');
 			}
 
 			fs.createReadStream(path, { start: fileOffset, encoding: 'utf8' }).on('data', chunk => {
@@ -149,15 +115,18 @@ const processLogLine = async (line, test = false) => {
 		});
 
 	// Auto updates
-	if (AUTO_UPDATE_ENABLED && AUTO_UPDATE_SCHEDULE && SERVER_ID !== 'development') await require('./scripts/services/updates.js')();
-	if (DISCORD_WEBHOOKS_ENABLED && DISCORD_WEBHOOKS_URL) await require('./scripts/services/summaries.js')();
-
-	// Final
-	if (SERVER_ID !== 'development') {
-		await sendWebhook(0, `[UFW-SpamVerify-Reporter](https://github.com/sefinek/UFW-SpamVerify-Reporter) has been successfully launched on the device \`${SERVER_ID}\`.`);
+	if (AUTO_UPDATE_ENABLED && AUTO_UPDATE_SCHEDULE && SERVER_ID !== 'development') {
+		await require('./scripts/services/updates.js')();
+	} else {
+		await require('./scripts/services/version.js')();
 	}
 
-	log(0, `Ready! Now monitoring: ${UFW_LOG_FILE}`);
+	// Summaries
+	if (DISCORD_WEBHOOKS_ENABLED && DISCORD_WEBHOOKS_URL) await require('./scripts/services/summaries.js')();
+
+	// Ready
+	await sendWebhook(`[${name}](${repoFullUrl}) has been successfully started on the device \`${SERVER_ID}\`.`, 0x59D267);
+	log(`Ready! Now monitoring: ${UFW_LOG_FILE}`, 1);
 	process.send && process.send('ready');
 })();
 
