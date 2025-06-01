32476e392eae818d82811e07df709e6aac9728e5
Sefinek <contact@sefinek.net>
2025-06-01 13:29:58 +0200

    Update `install.sh`

--- install.sh
+++ install.sh
@@ -6,23 +6,11 @@
 ##################################################################
 
 cat << "EOF"
-              _      _                            ___   ____    ____    ____
-             / \    | |__    _   _   ___    ___  |_ _| |  _ \  |  _ \  | __ )
-            / _ \   | '_ \  | | | | / __|  / _ \  | |  | |_) | | | | | |  _ \
-           / ___ \  | |_) | | |_| | \__ \ |  __/  | |  |  __/  | |_| | | |_) |
-          /_/   \_\_|_.__/ _ \__,_| |___/  \___| |___| |_|     |____/  |____/
-
-                  (_)_ __ | |_ ___  __ _ _ __ __ _| |_(_) ___  _ __
-                   | | '_ \| __/ _ \/ _` | '__/ _` | __| |/ _ \| '_ \
-                   | | | | | ||  __/ (_| | | | (_| | |_| | (_) | | | |
-                   |_|_| |_|\__\___|\__, |_|  \__,_|\__|_|\___/|_| |_|
-                                    |___/
-
-                 >> Made by sefinek.net || Last update: 18.05.2025 <<
-
 This installer will configure UFW-SpamVerify-Reporter, a tool that analyzes UFW logs and
 reports to SpamVerify the IP addresses that have violated firewall rules. Join my Discord
 server to stay updated on the latest changes and more: https://discord.gg/53DBjTuzgZ
+
+                 >> Made by sefinek.net || Last update: 1.06.2025 <<
 ============================================================================================
 
 EOF
