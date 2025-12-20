e979132e43259eef110b469f2a728287c06202fe
Sefinek <contact@sefinek.net>
2025-12-20 19:51:14 +0100

    Update config.default.js

--- config.default.js
+++ config.default.js
@@ -3,20 +3,20 @@ exports.MAIN = {
 	SERVER_ID: null, // Server identifier (e.g., 'hp-terminal', 'pl-cluster', 'de1'). Use 'development' for testing only. 'production' has no effect. Use null to leave it unset.
 	EXTENDED_LOGS: false, // Specifies whether the script should display additional information in the logs.
 	UFW_LOG_FILE: '/var/log/ufw.log',
-	CACHE_FILE: './tmp/reported-ips.cache',
+	CACHE_FILE: './tmp/ufw-spamverify-reporter.cache',
 
 	/* --------------------------- Network --------------------------- */
 	IP_ASSIGNMENT: 'dynamic', // IP assignment type: 'static' for a fixed IP, 'dynamic' if it may change over time.
 	IP_REFRESH_SCHEDULE: '0 */6 * * *', // Cron schedule for checking the public IP assigned by your ISP. Used only with dynamic IPs to prevent accidental self-reporting. If IP_ASSIGNMENT is set to 'static', the script will check your IP only once.
-	IPv6_SUPPORT: false, // IPv6 support: true if the device has a globally routable address assigned by the ISP.
+	IPv6_SUPPORT: true, // IPv6 support: true if the device has a globally routable address assigned by the ISP.
 
 	/* --------------------------- Reports --------------------------- */
 	SPAMVERIFY_API_KEY: '',
 	IP_REPORT_COOLDOWN: 12 * 60 * 60 * 1000, // Minimum time between reports of the same IP. Must be >= 15 minutes. Do not set values like 1 hour, as it wouldn't make sense due to rate limits.
 
 	/* --------------------------- Automatic Updates --------------------------- */
-	AUTO_UPDATE_ENABLED: false, // True to enable auto-update via 'git pull', false to disable.
-	AUTO_UPDATE_SCHEDULE: '0 15,17,18,20 * * *', // Cron schedule for automatic script updates. Default: every day at 15:00, 17:00, 18:00, 20:00
+	AUTO_UPDATE_ENABLED: false, // Set to true to enable automatic updates via "git pull", false to disable. This option is discouraged due to potential future incompatibilities. Enable it only if you actively monitor the server and want the latest version.
+	AUTO_UPDATE_SCHEDULE: '0 14,16,20 * * *', // Cron schedule for automatic updates. Default: every day at 14:00, 16:00, 20:00
 
 	/* --------------------------- Discord Webhooks --------------------------- */
 	DISCORD_WEBHOOK_ENABLED: false, // Enables sending Discord webhooks with error reports, execution status, and other events.
